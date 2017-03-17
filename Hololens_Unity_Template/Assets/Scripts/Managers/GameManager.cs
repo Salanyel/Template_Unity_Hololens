e88548336b54c56f9fcd892ceb87b2aacd30bdd5
Xyunn <maxime.devecchi@gmail.com>
2017-03-17 14:27:40 +0100

    Initializing the repository

---
+++ Hololens_Unity_Template/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Singleton used to manage the game/application
+/// </summary>
+public class GameManager : MonoBehaviour {
+
+}
